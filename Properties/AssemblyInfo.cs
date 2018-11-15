98b0c778572b6b9e9a6bccafe7bce83aeff91710
BogdanMateratskyy <bogdan.materatskyy@gmail.com>
2018-11-15 13:07:38 +0200

    add AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with the SQLCLR assembly.
+[assembly: AssemblyTitle("SQLStringAggregator")]
+[assembly: AssemblyDescription("Library for concatenating string")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Eleks")]
+[assembly: AssemblyProduct("Data Platform Camp Demo")]
+[assembly: AssemblyCopyright("Bogdan Materatskyi (c)")]
+[assembly: AssemblyTrademark("Bogdan Materatskyi")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- StringAgg.cs
+++ StringAgg.cs
@@ -1,34 +1,128 @@
 using System;
-using System.Data;
-using System.Data.SqlClient;
+using System.Collections.Generic;
 using System.Data.SqlTypes;
+using System.IO;
+using System.Text;
 using Microsoft.SqlServer.Server;
 
-[Serializable]
-[Microsoft.SqlServer.Server.SqlUserDefinedAggregate(Format.Native)]
-public struct StringAgg
+namespace Practical09
 {
-    public void Init()
+    public class Tuple
     {
-        // Put your code here
+        public string Item1;
+        public string Item2;
+
+        public Tuple(string value, string delimiter)
+        {
+            this.Item1 = value;
+            this.Item2 = delimiter;
+        }
     }
 
-    public void Accumulate(SqlString Value)
+    /// <summary>Represent custom aggregator for string summing in SQL server.</summary>
+    [Serializable]
+    [SqlUserDefinedAggregate(Format.UserDefined,
+        IsInvariantToDuplicates = false,
+        IsInvariantToNulls = true,
+        IsInvariantToOrder = false,
+        IsNullIfEmpty = true,
+        MaxByteSize = -1,
+        Name = "StrSumEx")]
+    public struct SqlStringSummator : IBinarySerialize, INullable
     {
-        // Put your code here
-    }
+        /// <summary>Constant value for initialization creating of internal state storage - elements counter.</summary>
+        private const int _StartAllocateSize = 102400;
 
-    public void Merge (StringAgg Group)
-    {
-        // Put your code here
-    }
+        /// <summary>Internal storage for characters count in resulted string.</summary>
+        private int _length;
 
-    public SqlString Terminate ()
-    {
-        // Put your code here
-        return new SqlString (string.Empty);
-    }
+        /// <summary>Internal state storage.</summary>
+        private List<Tuple> _state;
 
-    // This is a place-holder member field
-    public int _var1;
+        #region IBinarySerialize Members
+
+        /// <summary>Restore state of this structure from binary stream.</summary>
+        /// <param name="reader">Binary reader for getting data.</param>
+        public void Read(BinaryReader reader)
+        {
+            this._length = reader.ReadInt32();
+            int counter = reader.ReadInt32();
+            this._state =
+                new List<Tuple>(2 * counter - _StartAllocateSize < 0 ? _StartAllocateSize : 2 * counter);
+            for (int i = 0; i < counter; i++)
+                this._state.Add(new Tuple(reader.ReadString(), reader.ReadString()));
+        }
+
+        /// <summary>Write state of this structure to stream.</summary>
+        /// <param name="writer">Binary writer for writing data.</param>
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write(this._length);
+            writer.Write(this._state.Count);
+            foreach (Tuple tuple in this._state)
+            {
+                writer.Write(tuple.Item1);
+                writer.Write(tuple.Item2);
+            }
+        }
+
+        #endregion
+
+        #region INullable Members
+
+        /// <summary>Check if this structure results into null db object.</summary>
+        public Boolean IsNull
+        {
+            get { return this._state.Count == 0; }
+        }
+
+        #endregion
+
+        /// <summary>New structure initialization. It is always called before using.</summary>
+        public void Init()
+        {
+            this._length = 0;
+            this._state = new List<Tuple>(_StartAllocateSize);
+        }
+
+        /// <summary>Accumulate input values into internal state storage.</summary>
+        /// <param name="value">String value for summing.</param>
+        /// <param name="delimiter">Option delimiter between accumulated strings.</param>
+        public void Accumulate(
+            [SqlFacet(IsFixedLength = false, IsNullable = true, MaxSize = -1)] SqlString value,
+            [SqlFacet(IsFixedLength = false, IsNullable = true, MaxSize = -1)] SqlString delimiter)
+        {
+            if (!value.IsNull)
+            {
+                Tuple tuple = new Tuple(value.Value,
+                    delimiter.IsNull ? "" : delimiter.Value);
+                this._length += tuple.Item1.Length + tuple.Item2.Length;
+                this._state.Add(tuple);
+            }
+        }
+
+        /// <summary>Merge this structure with results for other summing groups.</summary>
+        /// <param name="group">Structure for merging.</param>
+        public void Merge(SqlStringSummator group)
+        {
+            this._length += group._length;
+            this._state.AddRange(group._state);
+        }
+
+        /// <summary>Finish summing.</summary>
+        /// <returns>SQL string with summing aggregations result.</returns>
+        [return: SqlFacet(IsFixedLength = false, IsNullable = true, MaxSize = -1)]
+        public SqlString Terminate()
+        {
+            if (this.IsNull)
+                return SqlString.Null;
+
+            StringBuilder builder = new StringBuilder(this._length);
+            for (int i = 0; i < this._state.Count - 1; i++)
+                builder.Append(this._state[i].Item1).Append(this._state[i].Item2);
+            if (this._state.Count > 0)
+                builder.Append(this._state[this._state.Count - 1].Item1);
+            return new SqlString(builder.ToString());
+        }
+    }
 }
